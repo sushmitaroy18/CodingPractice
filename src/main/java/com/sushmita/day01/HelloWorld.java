58019816d34e1f9886efa57eb67682b2a8b94c30
Sushmita Roy <rsushmita05@gmail.com>
2019-03-09 17:04:42 +0530

    Day 01- Hello World/Java

--- src/main/java/com/sushmita/day01/HelloWorld.java
+++ src/main/java/com/sushmita/day01/HelloWorld.java
@@ -2,7 +2,8 @@ package com.sushmita.day01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
 
